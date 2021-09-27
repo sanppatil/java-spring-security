eff50f267b28efc58031eb2c0d87fa05bbb7805f
Sandip Patil <sanppatil@gmail.com>
2021-09-26 22:11:22 -0400

    added changes

---
+++ src/test/java/org/enodation/springsecurityauthorization/SpringSecurityAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enodation.springsecurityauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
